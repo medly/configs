59fb35d37332278550dc15e1bb4f0f009f1e122f
gmukul01 <mukulbansal01@gmail.com>
2021-06-29 14:02:06 +0530

    feat: add file loader to support png, jpeg, gif in webpack config
    affects: @medly/webpack-config

--- packages/webpack-config/configs/webpack.common.js
+++ packages/webpack-config/configs/webpack.common.js
@@ -33,6 +33,14 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            },
+            {
                 test: /\.svg$/,
                 use: [
                     {
