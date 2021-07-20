d81e58097cd5dfa16e05b4ae26c197c9f7b405b1
gmukul01 <mukulbansal01@gmail.com>
2021-07-20 20:02:17 +0530

    feat: add types for weppack config's configure function
    affects: @medly/webpack-config

---
+++ packages/webpack-config/index.d.ts
@@ -0,0 +1,2 @@
+import { WebpackOptionsNormalized } from 'webpack';
+export declare const configure: (config: Partial<WebpackOptionsNormalized>) => (env: any, argv: any) => WebpackOptionsNormalized;
