516d2ad5c7b97aa1606e1ed5b6803fed4d837961
Mukul Bansal <mukulbansal01@gmail.com>
2022-12-23 13:55:31 +0000

    feat: add feature to pass babelConfig and tsConfig to rollup config
    affects: @medly/rollup-config

--- packages/rollup-config/index.d.ts
+++ packages/rollup-config/index.d.ts
@@ -1,2 +1,22 @@
 import { RollupOptions } from 'rollup';
-export declare const configure: (config: RollupOptions) => RollupOptions[];
+
+type Config = RollupOptions & {
+    /**
+     * The config file path that corresponds to the Babel's config. Defaults to searching for "bebel" keyword inside package.json
+     * or "babel.config.js" in the root folder.
+     *
+     * Eg. path.resolve("./configs/babel.config.js")
+     *
+     * Default: `undefined`
+     */
+    babelConfig: string;
+    /**
+     * The config file path that corresponds to the tsconfig. Defaults to searching for "tsconfig.json" in the root folder.
+     *
+     * Eg. path.resolve("./tsconfig.build.json")
+     *
+     * Default: `undefined`
+     */
+    tsConfig: string;
+};
+export declare const configure: (config: Config) => RollupOptions[];
--- packages/rollup-config/index.js
+++ packages/rollup-config/index.js
@@ -9,65 +9,70 @@ const { terser } = require('rollup-plugin-terser');
 
 const PACKAGE_ROOT_PATH = process.cwd(),
     SRC = path.join(PACKAGE_ROOT_PATH, './src'),
-    PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, './package.json')),
-    TS_CONFIG = path.join(PACKAGE_ROOT_PATH, './tsconfig.json');
+    PKG_JSON = require(path.join(PACKAGE_ROOT_PATH, './package.json'));
 
-const extensions = ['.ts', '.tsx', '.js', '.jsx'],
-    isTypescriptProject = fs.existsSync(TS_CONFIG);
+const extensions = ['.ts', '.tsx', '.js', '.jsx'];
 
-const configs = ['es', 'cjs'].map(format => {
-    return {
-        input: path.join(PACKAGE_ROOT_PATH, `./src/index.${isTypescriptProject ? 'ts' : 'js'}`),
-        preserveModules: true,
-        external: [
-            ...Object.keys(PKG_JSON.peerDependencies || {}),
-            ...Object.keys(PKG_JSON.dependencies || {}),
-            /@babel\/runtime/,
-            'react/jsx-runtime',
-            'react/jsx-dev-runtime'
-        ],
-        plugins: [
-            commonjs({
-                namedExports: {
-                    'react/jsx-runtime': ['jsx', 'jsxs', 'Fragment'],
-                    'react/jsx-dev-runtime': ['jsx', 'jsxs', 'jsxDEV']
-                }
-            }),
-            nodeResolve({
-                extensions,
-                customResolveOptions: { preserveSymlinks: false },
-                mainFields: ['module', 'main']
-            }),
-            babel({
-                ...PKG_JSON.babel,
-                extensions,
-                include: ['src/**/*'],
-                babelHelpers: 'runtime'
-            }),
-            ...(isTypescriptProject
-                ? [
-                      typescript({
-                          tsconfig: TS_CONFIG,
-                          rootDir: SRC,
-                          baseUrl: PACKAGE_ROOT_PATH,
-                          declaration: true,
-                          outDir: `dist/${format}`,
-                          exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.tsx', '**/*.stories.mdx', '**/docs/**'],
-                          include: ['src/**/*', 'module.d.ts'],
-                          module: 'esnext'
-                      })
-                  ]
-                : []),
-            terser(),
-            svgr({ native: true })
-        ],
-        output: {
-            format,
-            exports: 'auto',
-            dir: `dist/${format}`
-        }
-    };
-});
+const getConfigs = ({
+    babelConfig = path.join(PACKAGE_ROOT_PATH, './tsconfig.json'),
+    tsConfig = path.join(PACKAGE_ROOT_PATH, './babel.config.js')
+} = {}) =>
+    ['es', 'cjs'].map(format => {
+        const isTypescriptProject = fs.existsSync(tsConfig);
+
+        return {
+            input: path.join(PACKAGE_ROOT_PATH, `./src/index.${isTypescriptProject ? 'ts' : 'js'}`),
+            external: [
+                ...Object.keys(PKG_JSON.peerDependencies || {}),
+                ...Object.keys(PKG_JSON.dependencies || {}),
+                /@babel\/runtime/,
+                'react/jsx-runtime',
+                'react/jsx-dev-runtime'
+            ],
+            plugins: [
+                commonjs({
+                    namedExports: {
+                        'react/jsx-runtime': ['jsx', 'jsxs', 'Fragment'],
+                        'react/jsx-dev-runtime': ['jsx', 'jsxs', 'jsxDEV']
+                    }
+                }),
+                nodeResolve({
+                    extensions,
+                    customResolveOptions: { preserveSymlinks: false },
+                    mainFields: ['module', 'main']
+                }),
+                babel({
+                    ...(babelConfig ? { configFile: babelConfig } : PKG_JSON.babel),
+                    extensions,
+                    include: ['src/**/*'],
+                    babelHelpers: 'runtime'
+                }),
+                ...(isTypescriptProject
+                    ? [
+                          typescript({
+                              tsconfig: tsConfig,
+                              rootDir: SRC,
+                              baseUrl: PACKAGE_ROOT_PATH,
+                              declaration: true,
+                              outDir: `dist/${format}`,
+                              exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.tsx', '**/*.stories.mdx', '**/docs/**'],
+                              include: ['src/**/*', 'module.d.ts'],
+                              module: 'esnext'
+                          })
+                      ]
+                    : []),
+                terser(),
+                svgr({ native: true })
+            ],
+            output: {
+                format,
+                exports: 'auto',
+                interop: 'auto',
+                preserveModules: true,
+                dir: `dist/${format}`
+            }
+        };
+    });
 
 const merge = (target = {}, source) =>
     Object.entries(source).reduce(
@@ -82,7 +87,7 @@ const merge = (target = {}, source) =>
         { ...target }
     );
 
-const configure = (config = {}) => configs.map(cf => merge(cf, config));
+const configure = ({ babelConfig, tsConfig, ...config } = {}) => getConfigs({ babelConfig, tsConfig }).map(cf => merge(cf, config));
 
-module.exports = configs;
+module.exports = getConfigs();
 module.exports.configure = configure;
